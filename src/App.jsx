2c1722608e66c46684352aa7b06e53d4b8639212
Fernando Arvelos <fernandoarvelos1@gmail.com>
2023-06-12 14:31:40 -0300

    Corrigindo erros de publicação

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Home from './pages/Home';
 function App() {
   return (
     <Routes>
-      <Route path="/confereloterias" Component={ Home } exact />
+      <Route path="/confereloterias/" Component={ Home } />
     </Routes>
   );
 }
