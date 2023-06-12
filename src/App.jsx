91fcd72fb6ab121af589c4cd1b6c16f43524a0fb
Fernando Arvelos <fernandoarvelos1@gmail.com>
2023-06-12 14:20:04 -0300

    Publicando a página

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Home from './pages/Home';
 function App() {
   return (
     <Routes>
-      <Route path="/" Component={ Home } exact />
+      <Route path="/confereloterias" Component={ Home } exact />
     </Routes>
   );
 }
