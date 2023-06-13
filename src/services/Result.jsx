088359e2a4e06200e172ed76b562a42c0608902e
Fernando Arvelos <fernandoarvelos1@gmail.com>
2023-06-13 15:07:49 -0300

    Updates

--- src/services/Result.jsx
+++
@@ -1,8 +0,0 @@
-const resultGames = async (loteria, concurso) => {
-  const request = await fetch(`https://loteriascaixa-api.herokuapp.com/api/${loteria}/${concurso}`);
-  const requestJson = await request.json();
-  // console.log(requestJson);
-  return requestJson;
-};
-
-export default resultGames;
